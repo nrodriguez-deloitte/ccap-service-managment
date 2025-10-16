657744a21007c28d379a82e7886c107ff82a49da
Nino Rodriguez <oninross@gmail.com>
2025-10-16 16:45:01 +1100

    feat: migrate App component to TypeScript and update index file

---
+++ static/ccap/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to CCAP Service Management!</h1>
+      <p>This is your main application.</p>
+    </div>
+  );
+};
+
+export default App;
---
+++ static/ccap/src/index.tsx
@@ -0,0 +1,6 @@
+import ReactDOM from "react-dom";
+import App from "./App";
+
+import "@atlaskit/css-reset";
+
+ReactDOM.render(<App />, document.getElementById("root"));
