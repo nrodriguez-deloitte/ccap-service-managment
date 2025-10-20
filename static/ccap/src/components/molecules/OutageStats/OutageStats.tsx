38d1cbca1ed6e9b732928e9c651e661fad63f351
Nino Rodriguez <oninross@gmail.com>
2025-10-20 22:32:46 +1100

    refactor: remove loading state handling from OutageStats component for cleaner rendering

--- static/ccap/src/components/molecules/OutageStats/OutageStats.tsx
+++ static/ccap/src/components/molecules/OutageStats/OutageStats.tsx
@@ -1,45 +1,33 @@
-import { useDataOutage } from "@/app/contexts/DataContext";
-import { Card } from "@/components/ui/card";
+import { Card } from "../../ui/card";
 
 import "./OutageStats.styles.scss";
 import { IOutageStatProps } from "./OutageStats.types";
 
 /** Primary UI component for user interaction */
-export const OutageStats = (props: IOutageStatProps) => {
-  const { id, className } = props;
-  const { outageData, loading } = useDataOutage();
-
+export const OutageStats = ({
+  id,
+  className,
+  outageData,
+}: IOutageStatProps) => {
   return (
     <ul id={id} className={`outage-stats ${className}`}>
       <li>
         <Card className="outage-stats__card">
-          {loading ? (
-            <div className="animate-pulse h-6 w-full mb-2 rounded-full bg-gray-200" />
-          ) : (
-            <span className="--red">15</span>
-          )}
+          <span className="--red">15</span>
           Ongoing ACMA outages
         </Card>
       </li>
 
       <li>
         <Card className="outage-stats__card">
-          {loading ? (
-            <div className="animate-pulse h-6 w-full mb-2 rounded-full bg-gray-200" />
-          ) : (
-            <span>{outageData.ACMA_OUTAGES.toLocaleString("en-GB")}</span>
-          )}
+          <span>{outageData.ACMA_OUTAGES.toLocaleString("en-GB")}</span>
           Services impacted current ACMA Outages
         </Card>
       </li>
 
       <li>
         <Card className="outage-stats__card">
-          {loading ? (
-            <div className="animate-pulse h-6 w-full mb-2 rounded-full bg-gray-200" />
-          ) : (
-            <span>{outageData.UNPLANNED_OUTAGES.toLocaleString("en-GB")}</span>
-          )}
+          <span>{outageData.UNPLANNED_OUTAGES.toLocaleString("en-GB")}</span>
           Current unplanned outages
         </Card>
       </li>
--- static/ccap/src/components/molecules/OutageStats/OutageStats.types.ts
+++ static/ccap/src/components/molecules/OutageStats/OutageStats.types.ts
@@ -1,4 +1,7 @@
+import { IOutageDataProps } from "@/src/types/global.types";
+
 export interface IOutageStatProps {
   id?: string;
   className?: string;
+  outageData: IOutageDataProps;
 }
