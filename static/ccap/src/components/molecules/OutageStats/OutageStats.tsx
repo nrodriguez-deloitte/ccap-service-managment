04265861123d5105ecf04464dd1f28737328e3b4
Nino Rodriguez <oninross@gmail.com>
2025-10-21 12:50:54 +1100

    fix: add loading state handling to OutageStats component

--- static/ccap/src/components/molecules/OutageStats/OutageStats.tsx
+++ static/ccap/src/components/molecules/OutageStats/OutageStats.tsx
@@ -7,27 +7,40 @@ import { IOutageStatProps } from "./OutageStats.types";
 export const OutageStats = ({
   id,
   className,
+  loading = true,
   outageData,
 }: IOutageStatProps) => {
   return (
     <ul id={id} className={`outage-stats ${className}`}>
       <li>
         <Card className="outage-stats__card">
-          <span className="--red">15</span>
+          {loading ? (
+            <div className="animate-pulse h-6 w-full mb-2 rounded-full bg-gray-200" />
+          ) : (
+            <span className="--red">15</span>
+          )}
           Ongoing ACMA outages
         </Card>
       </li>
 
       <li>
         <Card className="outage-stats__card">
-          <span>{outageData.ACMA_OUTAGES.toLocaleString("en-GB")}</span>
+          {loading ? (
+            <div className="animate-pulse h-6 w-full mb-2 rounded-full bg-gray-200" />
+          ) : (
+            <span>{outageData.ACMA_OUTAGES.toLocaleString("en-GB")}</span>
+          )}
           Services impacted current ACMA Outages
         </Card>
       </li>
 
       <li>
         <Card className="outage-stats__card">
-          <span>{outageData.UNPLANNED_OUTAGES.toLocaleString("en-GB")}</span>
+          {loading ? (
+            <div className="animate-pulse h-6 w-full mb-2 rounded-full bg-gray-200" />
+          ) : (
+            <span>{outageData.UNPLANNED_OUTAGES.toLocaleString("en-GB")}</span>
+          )}
           Current unplanned outages
         </Card>
       </li>
--- static/ccap/src/components/molecules/OutageStats/OutageStats.types.ts
+++ static/ccap/src/components/molecules/OutageStats/OutageStats.types.ts
@@ -3,5 +3,6 @@ import { IOutageDataProps } from "@/src/types/global.types";
 export interface IOutageStatProps {
   id?: string;
   className?: string;
+  loading: boolean;
   outageData: IOutageDataProps;
 }
