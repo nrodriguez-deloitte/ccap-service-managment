ed967b7c6a076a7ed44d08ae3ea9f092239b678d
Nino Rodriguez <oninross@gmail.com>
2025-10-21 12:51:47 +1100

    fix: add loading state handling to MapPage component and update Map props

--- static/ccap/src/pages/Map.tsx
+++ static/ccap/src/pages/Map.tsx
@@ -1,18 +1,47 @@
+import { useEffect, useState } from "react";
 import { Map } from "../components/organisms/Map";
 import { IOutageDataProps } from "../types/global.types";
-
-type MapPageProps = {
-  outageData: IOutageDataProps;
-};
+import { DUMMY_CONSTANTS } from "../lib/constants";
 
 /**
  * MapPage displays the main map with outage data.
  * @param outageData - Data to pass to the Map component.
  */
-const MapPage: React.FC<MapPageProps> = ({ outageData }: MapPageProps) => {
+const MapPage: React.FC = () => {
+  const [outageData, setOutageData] = useState<IOutageDataProps>(
+    {} as IOutageDataProps
+  );
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  const fetchOutageData = async () => {
+    setLoading(true);
+    setError(null);
+    try {
+      const res = await fetch("https://mock.httpstatus.io/200");
+      if (!res.ok) throw new Error("Failed to fetch users");
+      // const data = await res.json();
+
+      setOutageData(DUMMY_CONSTANTS);
+    } catch (err) {
+      // Type guard to safely access error message
+      if (err instanceof Error) {
+        setError(err.message);
+      } else {
+        setError("Unknown error");
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchOutageData();
+  }, []);
+
   return (
     <div className="landing__content">
-      <Map id="main-map" outageData={outageData} />
+      <Map id="main-map" loading={loading} outageData={outageData} />
     </div>
   );
 };
